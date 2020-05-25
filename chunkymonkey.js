b147f3333fe6f634283a5c3c80d495a0160c727b
Shawn O'Brien <65410367+shawnob2123@users.noreply.github.com>
2020-05-25 17:14:54 -0400

    Create chunkymonkey.js

---
+++ chunkymonkey.js
@@ -0,0 +1,16 @@
+function chunkArrayInGroups(arr, size) {
+  let finalResult = []; // stores final array
+  let subArray =[]; // stores sub array that gets pushed into finalResult
+
+  for (let i in arr) {
+    if (subArray.length === size) { //sub array = size
+      finalResult.push(subArray);
+      subArray = []; //reset sub array
+    }
+    subArray.push(arr[i]) //push arr items into the sub array variable
+  }
+  finalResult.push(subArray);
+  return finalResult;
+}
+
+chunkArrayInGroups(["a", "b", "c", "d"], 2);
