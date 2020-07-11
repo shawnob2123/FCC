dacea8a9eb66985af97f51ec85aa5ee39f6a3857
Shawn O'Brien <65410367+shawnob2123@users.noreply.github.com>
2020-07-11 17:11:20 -0400

    Update frankensplice.js

--- frankensplice.js
+++ frankensplice.js
@@ -1,6 +1,6 @@
 function frankenSplice(arr1, arr2, n) {
   let arr2Copy = arr2.slice();
-for (var i = 0; i < arr1.length; i++) {
+for (let i = 0; i < arr1.length; i++) {
   arr2Copy.splice(n++, 0, arr1[i]);
 }
 return arr2Copy;
